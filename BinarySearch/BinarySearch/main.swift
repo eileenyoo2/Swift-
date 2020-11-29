f849547e47d034db52716d487aae5d65d06a83d6
eileenyoo2 <eileenyoo2@gmail.com>
2020-11-29 22:00:03 +0900

    BinarySearch

--- BinarySearch/BinarySearch/main.swift
+++ BinarySearch/BinarySearch/main.swift
@@ -12,17 +12,25 @@ func binarySearch(array: [Int], num: Int) -> Int? {
     var start = 0
     var end = array.count - 1
     
-    while start < end {
+    while start <= end {
         let mid = (start + end) / 2
+        let value = array[mid]
         
+        if value == num {
+            return mid
+        } else if value > num  {    // 내가 찾는 숫자(num)보다 추측 값(value)이 더 큰 경우, 다음엔 더 작게 추측
+            end = mid - 1
+        } else {                    // num이 추측 값(value) 보다 더 큰 경우, 다음엔 더 크게 추측
+            start = mid + 1
+        }
     }
-    return array
+    return nil
 }
 
-let input = readLine()!.components(separatedBy: " ")  // n(원소의 개수), target(찾고자 하는 문자열) 입력받기
-let inputChangeInt = input.map( {Int($0)!})    // 강제형변환 사용
+// input[0] : 원소의 개수 , input[1]: 찾고자 하는 문자열
+let input = readLine()!.components(separatedBy: " ").map { Int($0)!}
 
-var array = readLine()!.components(separatedBy: " ")
-var arrayChangeInt = array.map({Int($0)!})
-//print(str)
-//print(type(of: strChange[0]))
+var array = readLine()!.components(separatedBy: " ").map { Int($0)!}
+
+print(binarySearch(array: array, num: input[1]) ?? -1)  // swfit프로그래밍 p121 참고
+
