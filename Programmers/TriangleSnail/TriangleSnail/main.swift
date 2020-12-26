0de936e5e6e4a9180cfef0838c2aa8f855b0954f
eileenyoo2 <eileenyoo2@gmail.com>
2020-12-26 17:38:08 +0900

    삼각달팽이

--- Programmers/TriangleSnail/TriangleSnail/main.swift
+++ Programmers/TriangleSnail/TriangleSnail/main.swift
@@ -17,16 +17,16 @@ let dx = [1, 0, -1]
 let dy = [0, 1, -1]
 
 func solution(_ n:Int) -> [Int] {
-    
+
     var direction = 0       // 앞서 정의했던 dx, dy의 인덱스 역할
     var row = 0, col = 0    // 달팽이 채우기를 할때의 그리는 위치를 확인하기 위한 행, 열을 담당
     var num = 1             // 1,2,3,4,5,6 식으로 달팽이 채우기를 할때, 넘버링할 숫자
     var graph = [[Int]]()   // 삼각형 모양을 만들어주는 코드
-    
+
     (1 ... n).forEach {
         graph.append([Int](repeating: 0, count: $0))
     }
-    
+
     // n, n-1, n-2, ... 1 : 전환점까지의 거리
     (1 ... n).reversed().forEach {
         // 달팽이 채우기 방향에 맞게 숫자를 채우는 과정. 좌하단 방향으로 채우는 코드
@@ -36,17 +36,20 @@ func solution(_ n:Int) -> [Int] {
             col += dy[direction]
             num += 1
         }
-        
+
         graph[row][col] = num
         num += 1
         direction = (direction + 1) % 3 // 방향전환
         row += dx[direction]
         col += dy[direction]
     }
-    
+
     return graph.reduce(into: [Int]()) { $0.append(contentsOf: $1) }
 }
 
+//let graph = [[1,2,3],[4,5,6]]
+//
+//print(graph.reduce(into: [Int]()) { $0.append(contentsOf: $1) })
 
 
 
