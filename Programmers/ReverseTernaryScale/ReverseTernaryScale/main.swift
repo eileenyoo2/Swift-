d735d418334b3cd3e608b6f981a5d5fe75bbbd32
eileenyoo2 <eileenyoo2@gmail.com>
2020-12-28 11:59:03 +0900

    3진법 뒤집기

---
+++ Programmers/ReverseTernaryScale/ReverseTernaryScale/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  ReverseTernaryScale
+//
+//  Created by sapere4ude on 2020/12/28.
+//
+
+// [프로그래머스 3진법 뒤집기]
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    let flipToThree = String(n,radix: 3)
+    let answer = Int(String(flipToThree.reversed()),radix:3)!
+    return answer
+}
