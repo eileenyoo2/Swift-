be81716be14588a6be1512c531959dc608aab14c
eileenyoo2 <eileenyoo2@gmail.com>
2020-12-31 21:45:14 +0900

    기능개발

---
+++ Programmers/FunctionDevelopment/FunctionDevelopment/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  FunctionDevelopment
+//
+//  Created by sapere4ude on 2020/12/31.
+//
+
+// [프로그래머스 기능개발]
+
+import Foundation
+
+func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+    var days = [Int]()
+    
+    for i in 0 ..< progresses.count {
+        var x = 100 - progresses[i]
+        if speeds[i] *  {
+            
+        }
+    }
+    
+    return []
+}
+
