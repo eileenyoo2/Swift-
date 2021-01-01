d04029a74b8cb848067f6842f4f405e055ad1b2a
eileenyoo2 <eileenyoo2@gmail.com>
2021-01-01 11:32:35 +0900

    기능개발 코드작성

--- Programmers/FunctionDevelopment/FunctionDevelopment/main.swift
+++ Programmers/FunctionDevelopment/FunctionDevelopment/main.swift
@@ -10,15 +10,28 @@
 import Foundation
 
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    var days = [Int]()
+    var Q = [Int]()
+    var answer = [Int]()
     
-    for i in 0 ..< progresses.count {
-        var x = 100 - progresses[i]
-        if speeds[i] *  {
-            
+    // 반복문을 통해서 Q 배열에는 각각의 작업에 대한 작업완료 필요 일수가 들어감
+    for idx in progresses.indices {
+        Q.append((100 - progresses[idx] - 1) / speeds[idx] + 1)
+    }
+
+    var cur = 0
+    
+    while cur < Q.count {
+        let nowDelay = Q[cur]
+        var taskCnt = 0
+        while cur < Q.count && Q[cur] <= nowDelay {
+            taskCnt += 1
+            cur += 1
         }
+        answer.append(taskCnt)
     }
     
-    return []
+    return answer
 }
+
+print(solution([93,30,55], [1,30,5]))
 
