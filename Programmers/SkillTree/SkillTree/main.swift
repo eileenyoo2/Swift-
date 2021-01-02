f69c76ac0006b22e8c1d83b4abc8db47234db405
eileenyoo2 <eileenyoo2@gmail.com>
2021-01-02 21:23:37 +0900

    스킬트리 코드수정

--- Programmers/SkillTree/SkillTree/main.swift
+++ Programmers/SkillTree/SkillTree/main.swift
@@ -9,5 +9,32 @@
 
 import Foundation
 
+func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+    
+    var result = 0
+    
+    for i in skill_trees{
+        print("i:\(i)")
+        var currentSkill = ""
+        
+        for j in i {
+            print("j:\(j), i:\(i)")
+            if skill.contains(j) {
+                currentSkill += String(j)
+                print(currentSkill)
+            }
+        }
+        // hasPrefix : 문자열이 어떤 글자로 시작하는지 확인
+        if skill.hasPrefix(currentSkill) {
+            print("currentSkill:\(currentSkill)")
+            result += 1
+        }
+        print("--------------------------")
+    }
+    print(result)
+    return result
+}
+
+solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"])
 
 
