c50d39ea33357c083ba6d125825736253d46e855
eileenyoo2 <eileenyoo2@gmail.com>
2021-01-06 23:20:44 +0900

    카펫

---
+++ Programmers/Carpet/Carpet/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Carpet
+//
+//  Created by sapere4ude on 2021/01/05.
+//
+
+// [프로그래머스 카펫]
+
+import Foundation
+
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    let sum = brown + yellow
+    var x = Int()
+    var y = Int()
+    
+    for i in 1 ... sum {
+        if sum % i == 0 {
+            x = sum / i
+            y = i
+        }
+        
+        if (x - 2) * (y - 2) == yellow {
+            break
+        }
+    }
+    return [x,y]
+}
+
+
+print(solution(10, 2))
