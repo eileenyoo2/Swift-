84ed321aad2620fa0f6e626802c4a0ee9b27f341
sapere4ude <eileenyoo2@gmail.com>
2022-01-21 21:35:51 +0900

    - 프로그래머스 > 크레인 인형뽑기 게임

---
+++ Programmers/ClawMachineGame/ClawMachineGame/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  ClawMachineGame
+//
+//  Created by Kant on 2022/01/21.
+//
+
+import Foundation
+
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    
+    return 0
+}
