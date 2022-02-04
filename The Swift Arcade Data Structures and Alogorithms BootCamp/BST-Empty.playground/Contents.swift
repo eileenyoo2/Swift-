aaff9d8f2d963c37470f9a57f7d506e15b06891f
sapere4ude <eileenyoo2@gmail.com>
2022-02-04 22:23:02 +0900

    - Binary Search Tree

--- The Swift Arcade Data Structures and Alogorithms BootCamp/BST-Empty.playground/Contents.swift
+++ The Swift Arcade Data Structures and Alogorithms BootCamp/BST-Empty.playground/Contents.swift
@@ -17,16 +17,46 @@ class Node {
     init(_ key: Int) {
         self.key = key
     }
+    // 최소값을 구하는 코드
+    var min: Node {
+        if left == nil {
+            return self
+        } else {
+            return left!.min
+        }
+    }
+    // 나혼자 만든 코드
+    var max: Node {
+        if right == nil {
+            return self
+        } else {
+            return right!.max
+        }
+    }
 }
 
 class BST {
     var root: Node?
 
     func insert(key: Int) {
+        root = insertItem(root, key)
     }
-
+    
+    // insert 5 3 2 4 7 6 8
     private func insertItem(_ node: Node?, _ key: Int) -> Node {
-        return Node(key)
+        guard let node = node else {    // node 값이 Null 이라면 guard 문을 타고 종료된다.
+            let node = Node(key)
+            return node
+        }
+        
+        if key < node.key {
+            node.left = insertItem(node.left, key)
+        }
+        if key > node.key {
+            node.right = insertItem(node.right, key)
+        }
+        
+        return node
     }
 
     func find(key: Int) -> Int? {
@@ -38,7 +68,7 @@ class BST {
 
     private func find(_ node: Node?, _ key: Int) -> Node? {
         guard let node = node else { return nil }
-        
+        // 이 부분을 통해서 찾고자 하는 node 를 알 수 있다.
         if node.key == key {
             return node
         } else if key < node.key {
@@ -59,9 +89,46 @@ class BST {
     }
 
     func delete(key: Int) {
+        guard let _ = root else { return }
+        root = delete(&root, key)
     }
     
+    // inout 변수는 함수 내에서 값을 변화시키고 전달할때 사용하는 것
     private func delete(_  node: inout Node?, _ key: Int) -> Node? {
+        guard let nd = node else { return nil }
+        
+        if key < nd.key {
+            nd.left = delete(&nd.left, key)
+        } else if key > nd.key {
+            nd.right = delete(&nd.right, key)
+        } else {
+            // This is the node we want to delete.
+            
+            // Case 1: No child
+            if nd.left == nil && nd.right == nil {
+                node = nil
+            }
+            
+            // Case 2: One child
+            else if nd.left == nil {
+                node = nd.right
+            }
+            else if nd.right == nil {
+                node = nd.left
+            }
+            
+            // Case 3: Two Children
+            else {
+                // Find the minimum node on the right (could also find max on the left)
+                let minRight = findMin(nd.right!)
+                
+                // Duplicate it by copying its value here
+                node!.key = minRight.key
+                
+                // Now go ahead and delete the node we just duplicated (same key)
+                node!.right = delete(&node!.right, node!.key)
+            }
+        }
         return nil
     }
 
@@ -95,6 +162,7 @@ class BST {
         """
         
         print(str)
+        print(root?.min)    // 노드의 최소값 확인
     }
 }
 
